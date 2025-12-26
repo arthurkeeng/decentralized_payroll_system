d9ad1fce8a800b3b9e40d590e46f02b0462f9c29
arthurkeeng <keengarthurchima@gmail.com>
2025-12-26 17:58:32 +0100

    extension configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { WagmiProvider } from "wagmi";
 import { QueryClient , QueryClientProvider } from "@tanstack/react-query";
 import { wagmiConfig } from "@/lib/wagmi";
 import type  {Metadata} from "next"
-import Providers from "./providers.tsx"
+import Providers from "./providers"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
